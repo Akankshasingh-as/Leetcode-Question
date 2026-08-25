d70a1fe7a28032f24069e4aff43f547687139976
Akankshasingh-as <akanksha5109singh@gmail.com>
2026-08-26 00:18:49 +0530

    Time: 2 ms (99.34%), Space: 46.9 MB (21.54%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -21,6 +21,6 @@ class Solution {
     }
     public boolean isVowel(char ch){
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
-        
     }
+
 }
