743a2890abc55f8c9f678b45c0f9bf7736ff25f6
Akankshasingh-as <akanksha5109singh@gmail.com>
2026-09-16 12:16:30 +0530

    Time: 4 ms (96.73%), Space: 70.6 MB (41.27%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        for(int i=0;i<nums.length;i++){
+       /* for(int i=0;i<nums.length;i++){
             int index = Math.abs(nums[i])-1;
             nums[index] = -Math.abs(nums[index]);
         }
@@ -11,6 +11,19 @@ class Solution {
             }
         }
         return result;
+        */
+
+         List<Integer> ans = new ArrayList<>();
+        int[] freq = new int[nums.length + 1];
+        for (int num : nums) {
+            freq[num]++;
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            if (freq[i] == 0) {
+                ans.add(i);
+            }
+        }
+        return ans;
         
     }
 }
